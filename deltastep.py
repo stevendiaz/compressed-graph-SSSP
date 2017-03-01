9b1bca9ea12d506272ffd54eb926d5f20d3bc76b
Estevan Diaz <sdiaz@cs.utexas.edu>
2017-02-28 23:05:55 -0600

    working delta_step for d=1 in python

--- deltastep.py
+++ deltastep.py
@@ -1,3 +1,4 @@
+import math
 INT_MAX = 2147483647
 
 def parse_input():
@@ -12,50 +13,105 @@ def parse_input():
             line.pop(0)
         u, v, weight = [int(x) for x in line]
         matrix.set(u, v, weight)
-    #print('A[1][0] = %d' % matrix.get(1, 0))
-    #print('A[1][1] = %d' % matrix.get(1, 1))
-    #print('A[2][2] = %d' % matrix.get(2, 2))
-    #print('A[3][1] = %d' % matrix.get(3, 1))
-    #matrix.debug_info()
-    print matrix.iterate()
-    matrix.print_dimacs()
-    matrix.print_node_labels()
-    print matrix.largest_out_degree()
+    delta_step(matrix, 1)
 
 def delta_step(graph, delta):
-    pass
+    heavy = set()
+    light = set()
+    for vertex in graph.iterate():
+        cost = vertex[2]
+        if cost > delta:
+            heavy.add(vertex)
+        else:
+            light.add(vertex)
+        graph.set_tent(vertex[0], INT_MAX)
+    graph.set_tent(0, 0)
+
+    buckets = make_buckets(graph, delta)
+    while has_elements(buckets):
+        S = set()
+        i = get_index(buckets)
+        while len(buckets[i]) > 0:
+            req = light_matches(graph, buckets[i], light)
+            S = S.union(buckets[i])
+            buckets[i] = set()
+            for e in req:
+                relax(graph, buckets, e[0], e[1], delta)
+        req = heavy_matches(graph, S, heavy)
+        for e in req:
+            relax(graph, buckets, e[0], e[1], delta)
+    graph.print_node_labels()
+
+def relax(graph, buckets, w, d, delta):
+    tent_cost = graph.get_tent(w)
+    if d < tent_cost:
+        graph.set_tent(w, d)
+        i = math.floor(tent_cost/delta)
+        buckets[i].remove(w)
+        new_idx = math.floor(d/delta)
+        if new_idx not in buckets:
+            buckets[new_idx] = set([w])
+        else:
+            buckets[new_idx].add(w)
+
+def get_index(buckets):
+    for key in buckets:
+        if len(buckets[key]) > 0:
+            return key
+
+def make_buckets(graph, delta):
+    B = {}
+    for v in graph.iterate():
+        i = math.floor(graph.get_tent(v[0])/delta)
+        if i not in B:
+            B[i] = set([v[0]])
+        else:
+            B[i].add(v[0])
+    return B
+
+def heavy_matches(graph, S, heavy):
+    result = set()
+    for edge in heavy:
+        if edge[0] in S:
+           result.add((edge[1], graph.get_tent(edge[0]) + edge[2]))
+    return result
+
+def light_matches(graph, bucket, light):
+    result = set()
+    for edge in light:
+        if edge[0] in bucket:
+            result.add((edge[1], graph.get_tent(edge[0]) + edge[2]))
+    return result
+
+def has_elements(buckets):
+    """
+    Return False if for all the entry in buckets, the set is empty
+    """
+    for i in buckets.keys():
+        if len(buckets[i]) != 0:
+            return True
+    return False
 
 class CSRImpl:
     def __init__(self, numRows, numCols):
         self.value = []
         self.IA = [0] * (numRows + 1)
         self.JA = []
-        self.node_labels = [INT_MAX] * numRows
-        self.node_labels[0] = 0
+        self.node_labels = [0] * numRows
         self.numRows = numRows
         self.numCols = numCols
-        print('rows: %d' % numRows)
     def get(self, x, y):
         entries_in_row = self.IA[x + 1] - self.IA[x]
-        #print('val[%d]: %d' % (self.IA[x], self.value[self.IA[x] + y]))
-        #print('x: %d y: %d' % (x, y))
         for i in xrange(self.IA[x], self.IA[x+1]):
             if self.JA[i] == y:
                 return self.value[i]
-            #print('val[%d]: %d' % (i, self.value[i]))
-        #offset = self.JA[entries_in_row] + y
         return 0
     def set(self, x, y, v):
         for i in range(x+1, self.numRows+1):
-            print('IA size line27: %d' % len(self.IA))
-            print('index of IA: %d' % i)
             self.IA[i] += 1
         previous_row_values_count = self.IA[x]
         inserted = False
-        print('i: %d j: %d' % (previous_row_values_count, self.IA[x+1] - 1))
         for j in range(previous_row_values_count, self.IA[x+1]-1):
-            print('Loop enter')
-            print('Index: %d JA size: %d Val size: %d' % (j, len(self.JA), len(self.value)))
             if self.JA[j] > y:
                 self.JA.insert(j, y)
                 self.value.insert(j, v)
@@ -66,12 +122,9 @@ class CSRImpl:
                 self.value[j] = v
                 break
         if not inserted:
-            print('Line 62: index: %d JA size: %d Val size: %d' % (self.IA[x+1]-1, len(self.JA), len(self.value)))
             self.JA.insert(self.IA[x+1]-1,y)
             self.value.insert(self.IA[x+1]-1, v)
-        print("val: %s " % self.value)
-        print("IA: %s " % self.IA)
-        print("JA: %s " % self.JA)
+
     def iterate(self):
         result = [] # a list of triple (row, col, value)
         for i,v in enumerate(self.IA):
@@ -93,8 +146,8 @@ class CSRImpl:
             print 'a %d %d %d' % (edge[0], edge[1], edge[2])
 
     def print_node_labels(self):
-        for i,v in enumerate(self.node_labels):
-            print('%d -> %d' % (i, v))
+        for i in xrange(len(self.node_labels)):
+            print('%d -> %d' % (i, self.node_labels[i]))
 
     def largest_out_degree(self):
         old_degree = -1
@@ -106,11 +159,14 @@ class CSRImpl:
                 old_degree = curr_degree
         return row
 
-    def cost(u, v):
+    def cost(self, u, v):
         return self.get(u,v)
 
-    def tent(u):
+    def get_tent(self, u):
         return self.node_labels[u]
+
+    def set_tent(self, u, value):
+        self.node_labels[u] = value
 
     def debug_info(self):
         print 'value ', self.value
