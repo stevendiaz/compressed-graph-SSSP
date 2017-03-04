67cfd76bdfe06ede51c0d89592a19c210a402d35
Eryn Li <erynli@utexas.edu>
2017-03-04 14:49:14 -0600

    rewrote split using strtok

--- csr_cpp/main.cpp
+++ csr_cpp/main.cpp
@@ -10,21 +10,16 @@
 
 const int MAX_CHARS_PER_LINE = 512;
 
-// // Credit:
-// http://ysonggit.github.io/coding/2014/12/16/split-a-string-using-c.html
-vector<string> split(const string &s, char delim) {
-    vector<string> v;
-    auto i = 0;
-    auto pos = s.find(delim);
-    while (pos != string::npos) {
-        v.push_back(s.substr(i, pos - i));
-        i = ++pos;
-        pos = s.find(delim, pos);
+vector<string> split(char* s) {
+    char * pch;
+    vector<string> ss;
+    pch = strtok (s ," ");
 
-        if (pos == string::npos)
-            v.push_back(s.substr(i, s.length()));
+    while (pch != NULL) {
+        ss.push_back(pch);
+        pch = strtok (NULL, " ,.-");
     }
-    return v;
+    return ss;
 }
 
 int32_t stoi32(string s){
@@ -35,7 +30,7 @@ CSR* parseInput(){
     //Extract info from first line
     char buffer[MAX_CHARS_PER_LINE];
     cin.getline(buffer, MAX_CHARS_PER_LINE);
-    vector<string> line = split(buffer, ' ');
+    vector<string> line = split(buffer);
     int32_t size = stoi32(line[2]);
     int32_t edges = stoi32((line[3]));
 
@@ -45,7 +40,7 @@ CSR* parseInput(){
     CSR* csr = new CSR(size);
     for(int i = 0; i < edges; ++i){
         cin.getline(buffer, MAX_CHARS_PER_LINE);
-        line = split(buffer, ' ');
+        line = split(buffer);
         if(line[0] == "a") {
             csr->put(stoi32(line[1]), stoi32(line[2]), stoi32(line[3]));
         }
