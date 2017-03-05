52b10e7ae32be4d5d2e4bc1a5235ed15fe464454
Steven Diaz <stevendiaz@utexas.edu>
2017-03-05 11:16:40 -0600

    adjusted slurm script

--- src/chaoticExperiment.cpp
+++ src/chaoticExperiment.cpp
@@ -44,19 +44,28 @@ CSR parseInput(){
 
 
 int main(){
-	CSR csr = parseInput();
+    CSR csr = parseInput();
 
-	cout << "Random seed: 10" << endl;
+    cout << "Random seed: 10" << endl;
+    clock_t t;
     DeltaStep deltaStep = DeltaStep(csr, INT_MAX, 10);
     deltaStep.run(false, true);
+    t = clock() - t;
+    cout << "Delta step algorithm with choatic relaxation took " << ((float)t)/CLOCKS_PER_SEC << " seconds" << endl; 
 
     cout << "Random seed: 20" << endl;
+    clock_t x;
     deltaStep = DeltaStep(csr, INT_MAX, 20);
     deltaStep.run(false, true);
+    x = clock() - x;
+    cout << "Delta step algorithm with choatic relaxation took " << ((float)x)/CLOCKS_PER_SEC << " seconds" << endl; 
 
     cout << "Random seed: 50" << endl;
+    clock_t y;
     deltaStep = DeltaStep(csr, INT_MAX, 50);
     deltaStep.run(false, true);
+    y = clock() - y;
+    cout << "Delta step algorithm with choatic relaxation took " << ((float)y)/CLOCKS_PER_SEC << " seconds" << endl; 
 
 
     return 0;
--- src/deltaStepExperiment.cpp
+++ src/deltaStepExperiment.cpp
@@ -47,7 +47,7 @@ int main(){
 	CSR csr = parseInput();
 
 
-    int32_t step = INT_MAX/20;
+    int32_t step = INT_MAX/5;
     for(int i = 1; i <= INT_MAX; i += step){
         cout << "step = " << i << endl;
         DeltaStep deltaStep = DeltaStep(csr, i, 10);
