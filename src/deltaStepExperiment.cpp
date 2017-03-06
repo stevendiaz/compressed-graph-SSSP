10dd1f1159985dca333b002c0d294b053278fa68
Eryn Li <erynli@utexas.edu>
2017-03-05 19:04:50 -0600

    changed step range for delta experiment

--- src/deltaStepExperiment.cpp
+++ src/deltaStepExperiment.cpp
@@ -7,11 +7,9 @@ int main(){
     Parser p = Parser();
 	CSR csr = p.parseInput();
 
-    int32_t step = 1;
-    int32_t increment = INT_MAX/5;
-    for(int i = 0; i < 6; ++i){
-        cout << "step = " << step + i * increment << endl;
-        DeltaStep deltaStep = DeltaStep(csr, step + i * increment, 10);
+    for(int i = 1; i < 120; i+=10){
+        cout << "step = " << i << endl;
+        DeltaStep deltaStep = DeltaStep(csr, i, 10);
         deltaStep.run(true, true);
         cout << endl;
     }
