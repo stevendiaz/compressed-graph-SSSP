784faf4b80b00a360d715df78ae241e3b1e90acf
Eryn Li <erynli@utexas.edu>
2017-03-06 23:48:06 -0600

    implemented put and update for CSR

--- src/CSR.cpp
+++ src/CSR.cpp
@@ -1,66 +1,108 @@
 #include "CSR.h"
 
 /* CSRImpl Class Methods */
-CSR::CSR(int32_t size) : size(size + 1) {
+CSR::CSR(int32_t size) : size(size + 1), currSrc(1), NNZ(0) {
     size += 1;
     value = vector<int32_t>();
-    IA = vector<int32_t>(size + 1, 0);
+    IA = vector<int32_t>({0});
     JA = vector<int32_t>();
-    seenNodes = map < csrTuple, int32_t > ();
+    seenNodes = vector<int32_t > (size + 2, -1);
     nodeLabels = vector<long>(size, INT_MAX);
     relaxMap = map<int32_t, set<int32_t>>();
+    // mtx = vector<vector<int32_t> >(edges);
+    tempJA = vector<int32_t>();
 }
 
-void CSR::updateValue(int32_t x, int32_t y, int32_t val) {
-    int32_t preVRowVal = IA[x];
-    bool inserted = false;
-    auto jit = JA.begin();
-    auto vit = value.begin();
+// void CSR::updateValue(int32_t x, int32_t y, int32_t val) {
+//     int32_t preVRowVal = IA[x];
+//     bool inserted = false;
+//     auto jit = JA.begin();
+//     auto vit = value.begin();
 
-    for (int j = preVRowVal; j < IA[x + 1] - 1; ++j) {
-        if (JA.at(j) > y) {
-            JA.insert(jit + j, y);
-            value.insert(vit + j, val);
-            inserted = true;
-            break;
-        } else if (JA.at(j) == y) {
-            inserted = true;
-            value.at(j) = val;
-            break;
-        }
+//     for (int j = preVRowVal; j < IA[x + 1] - 1; ++j) {
+//         if (JA.at(j) > y) {
+//             JA.insert(jit + j, y);
+//             value.insert(vit + j, val);
+//             inserted = true;
+//             break;
+//         } else if (JA.at(j) == y) {
+//             inserted = true;
+//             value.at(j) = val;
+//             break;
+//         }
+//     }
+
+//     //Fall safe
+//     if (!inserted) {
+//         JA.insert(jit + IA[x + 1] - 1, y);
+//         value.insert(vit + IA[x + 1] - 1, val);
+//     }
+
+//     //Mark (x, y) visited
+//     csrTuple coordinate(x, y);
+//     seenNodes[coordinate] = val;
+// }
+
+// int32_t CSR::get(int32_t x, int32_t y) {
+//     for (int i = IA[x]; i < IA[x + 1]; ++i) {
+//         if (JA[i] == y) return value[i];
+//     }
+//     return 0;
+// // }
+
+void CSR::update(int32_t x){
+    IA.push_back(NNZ);
+    currSrc = x;
+    sort(tempJA.begin(), tempJA.end());
+    JA.insert(JA.end(), tempJA.begin(), tempJA.end());
+    // cout << "size of JA is " << JA.size() << endl;
+
+    for(auto it = tempJA.begin(); it != tempJA.end(); ++it){
+        value.push_back(seenNodes[*it]);
     }
-
-    //Fall safe
-    if (!inserted) {
-        JA.insert(jit + IA[x + 1] - 1, y);
-        value.insert(vit + IA[x + 1] - 1, val);
-    }
-
-    //Mark (x, y) visited
-    csrTuple coordinate(x, y);
-    seenNodes[coordinate] = val;
-}
-
-int32_t CSR::get(int32_t x, int32_t y) {
-    for (int i = IA[x]; i < IA[x + 1]; ++i) {
-        if (JA[i] == y) return value[i];
-    }
-    return 0;
 }
 
 void CSR::put(int32_t x, int32_t y, int32_t val) {
-    csrTuple coordinate(x, y);
     if(relaxMap.find(x) == relaxMap.end()) relaxMap[x] = set<int32_t>({y});
     else relaxMap[x].insert(y);
 
-    if (seenNodes.find(coordinate) == seenNodes.end()) {
-//        cout << x << " " << y << endl;
-        for (int i = x + 1; i <= size; ++i){
-            ++IA[i];
-        }
-        updateValue(x, y, val);
-    } else if (val > get(x, y)) updateValue(x, y, val);
+    // cout << "x = " << x << ", y = " << y << endl;
+    
+    if(currSrc < x){
+        update(x);
+        seenNodes = vector<int32_t>(size + 2, -1);
+        tempJA = vector<int32_t>();
+        debugInfo();
+    }
+
+    cout << "x = " << x << ", y = " << y << endl;
+    
+    if(seenNodes[y] == -1){
+        ++NNZ;
+        tempJA.push_back(y);
+        seenNodes[y] = val;
+    } else {
+        if(seenNodes[y] < val)
+        value.at(y) = val;
+    }
 }
+
+// void CSR::put(int32_t x, int32_t y, int32_t val){
+//      if(currSrc < x){
+//         update(x);
+//         seenNodes = vector<int32_t>(size + 2, -1);
+//     }
+
+//     if(seenNodes[y] == -1){
+//         ++NNZ;
+//         JA.push_back(y);
+//         value.push_back(val);
+//         seenNodes[y] = val;
+//     } else {
+//         if(seenNodes[y] < val)
+//         value.at(y) = val;
+//     }
+// }
 
 vector <vector<int32_t>> CSR::iterate() {
     vector <vector<int32_t>> result;
--- src/CSR.h
+++ src/CSR.h
@@ -5,6 +5,7 @@
 #include <map>
 #include <set>
 #include <iostream>
+#include <algorithm>
 
 #ifndef CSR_h
 #define CSR_h
@@ -19,11 +20,13 @@ private:
     vector<int32_t> value;
     vector<int32_t> IA;
     vector<int32_t> JA;
-    map<csrTuple, int32_t> seenNodes;
+    vector<int32_t> seenNodes;
+    int32_t currSrc;
+    int32_t NNZ;
     vector<long> nodeLabels;
+    // vector<vector<int32_t> > mtx;
+    vector<int32_t> tempJA;
     map<int32_t, set<int32_t>> relaxMap;
-
-
     void updateValue(int32_t x, int32_t y, int32_t val);
 
 public:
@@ -39,6 +42,7 @@ public:
     void debugInfo();
     bool nodeFullyRelaxed(int32_t node);
     void relaxNode(int32_t src, int32_t dest);
+    void update(int32_t x);
 };
 
 #endif
